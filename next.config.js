016f15b0d94630aff44191c3ad02990208bbaf22
Mark Vadeika <markvadeika@gmail.com>
2020-09-04 19:22:29 -0400

    removed fs config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 const withSass = require("@zeit/next-sass");
 const withImages = require("next-images");
 module.exports = withSass(
-    withImages({
-        webpack: (config, { isServer }) => {
-            if (!isServer) {}
-            config.node = {
-                fs: "empty"
-            };
-            return config;
-        },
-    })
+    withImages()
 );
